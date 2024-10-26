14b9c054eca639d92dcd4e8f85bc17c3b728619e
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2024-10-25 22:19:16 -0300

    Primeiro commit

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Git Finder</h1>
+    </header>
+  );
+};
+
+export {Header}
---
+++ src/pages/home/index.js
@@ -0,0 +1,25 @@
+import {Header} from '../../components/header';
+import background from '../../assets/background.png';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div className="conteudo">
+        <img src={background} className="background" alt="Github"/>
+        <div className='info'>
+          <div>
+            <input name='usuario' placeholder='@username'/>
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
